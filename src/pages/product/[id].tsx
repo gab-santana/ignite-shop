3c0f11406e049b5d02df35a43803689d56d3be0e
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-06-22 17:14:10 -0300

    structuring product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,21 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
-export function Product() {
+export default function Product() {
   const { query } = useRouter()
   return (
-    <h1>products</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi neque, sapiente soluta ea sequi ipsa velit deleniti temporibus iste ipsam! Corporis ipsam ab vitae impedit minima voluptatibus eaque doloremque temporibus.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
