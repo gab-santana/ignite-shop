6236f82111ed0e02eee0d83422d856c0fb9c168e
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-06-22 21:53:20 -0300

    checkout stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,11 +12,16 @@ interface ProdutcProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProdutcProps) {
-  const { query } = useRouter()
+
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -29,7 +34,7 @@ export default function Product({ product }: ProdutcProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -64,7 +69,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           style: 'currency',
           currency: 'BRL',
         }).format(price.unit_amount as number / 100),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1, //1hour
